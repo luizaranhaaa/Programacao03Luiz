a575158322272f6f7666909c96a5d526fabfe26f
Peterson Luiz <petersonluiz2008@gmail.com>
2025-08-08 18:48:02 -0300

    first commit

---
+++ Projeto1/src/projeto1/Projeto1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto1;
+
+/**
+ *
+ * @author info2522
+ */
+public class Projeto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
